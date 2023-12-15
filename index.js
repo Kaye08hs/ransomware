3414ff86e0c0b917a23e111684fefaacc15cb4ba
Kaye08hs <94820986+Kaye08hs@users.noreply.github.com>
2023-12-15 17:45:26 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+window.addEventListener('scroll', function () {
+    var nav = document.querySelector('nav');
+    if (window.scrollY > 50) {
+        nav.classList.add('sticky');
+    } else {
+        nav.classList.remove('sticky');
+    }
+});
+
 
